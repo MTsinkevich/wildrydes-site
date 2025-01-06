1a86e36d14494daa3e0aba089deed25e8cc7deda
Maks Tvich <106677138+MTsinkevich@users.noreply.github.com>
2025-01-06 16:47:55 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_DkWu2rCNO
+        userPoolClientId: '', // e.g. 7l3dq91ih8lpms2c4fouh94rjo
         region: '' // e.g. us-east-2
     },
     api: {
