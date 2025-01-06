31e28c8b6127c71c71241a86051f92659dc4265c
Maks Tvich <106677138+MTsinkevich@users.noreply.github.com>
2025-01-06 16:58:20 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_DkWu2rCNO
-        userPoolClientId: '', // e.g. 7l3dq91ih8lpms2c4fouh94rjo
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_DkWu2rCNO', // e.g. us-east-2_DkWu2rCNO
+        userPoolClientId: '7l3dq91ih8lpms2c4fouh94rjo', // e.g. 7l3dq91ih8lpms2c4fouh94rjo
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
