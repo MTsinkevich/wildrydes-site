c16ea1bca4975631611b527b56055142a4cc97d2
Maks Tvich <106677138+MTsinkevich@users.noreply.github.com>
2025-01-06 17:06:02 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_DkWu2rCNO', // e.g. us-east-2_DkWu2rCNO
-        userPoolClientId: '7l3dq91ih8lpms2c4fouh94rjo', // e.g. 7l3dq91ih8lpms2c4fouh94rjo
+        userPoolClientId: '52uea6ejp94sleh68dilhu4hd6', // e.g. 52uea6ejp94sleh68dilhu4hd6
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
